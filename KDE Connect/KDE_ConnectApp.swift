23cdd21e0aa40ada846b8e6de2a901cb0b750e70
Ruixuan Tu <turx2003@gmail.com>
2022-05-12 00:44:19 -0500

    Initial Commit

---
+++ KDE Connect/KDE_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KDE_ConnectApp.swift
+//  KDE Connect
+//
+//  Created by 涂睿轩 on 2022/05/12.
+//
+
+import SwiftUI
+
+@main
+struct KDE_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KDE ConnectUITests/KDE_ConnectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KDE_ConnectUITestsLaunchTests.swift
+//  KDE ConnectUITests
+//
+//  Created by 涂睿轩 on 2022/05/12.
+//
+
+import XCTest
+
+class KDE_ConnectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
