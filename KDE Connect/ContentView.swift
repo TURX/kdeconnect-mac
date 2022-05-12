72a1fc13225e6fc5fe579cfa99be2a5e17ff8406
Ruixuan Tu <turx2003@gmail.com>
2022-05-12 00:44:19 -0500

    Initial Commit

---
+++ KDE Connect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KDE Connect
+//
+//  Created by 涂睿轩 on 2022/05/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
